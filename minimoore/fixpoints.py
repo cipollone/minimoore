6b46c80910c06652e5b12f00e31cfe4df33f4d49
Roberto Cipollone <cipollone.rt@gmail.com>
2021-04-19 22:22:37 +0200

    Small utilities for fixpoint methods.

--- minimoore/fixpoints.py
+++ minimoore/fixpoints.py
@@ -1,7 +1,6 @@
 """Implementation of fixpoint algorithms for functions on sets."""
 
-from typing import TypeVar, Callable, Set, Optional
-
+from typing import Callable, Generic, Optional, Set, TypeVar
 
 # Types
 ElementType = TypeVar("ElementType")
@@ -59,3 +58,39 @@ def greatest_fixpoint(
     :return: the greatest fixpoint set of fn.
     """
     return reach_fixpoint(fn, universe)
+
+
+class Union(Generic[ElementType]):
+    """Function wrapper: union.
+
+    Any element that is returned by the input function is added to the set.
+    This can be passed to fixpoint methods.
+    The input function shouln't have side-effects on the set.
+    """
+    def __init__(self, fn: FunctionType[ElementType]):
+        """Initialize; see class docstring."""
+        self.fn = fn
+
+    def __call__(self, x: Set[ElementType]) -> Set[ElementType]:
+        """Modify the input set."""
+        elements = self.fn(x)
+        x.update(elements)
+        return x
+
+
+class Intersection(Generic[ElementType]):
+    """Function wrapper: intersection.
+
+    Any element that is returned by the input function is removed from the set,
+    if present. This can be passed to fixpoint methods.
+    The input function shouln't have side-effects on the set.
+    """
+    def __init__(self, fn: FunctionType[ElementType]):
+        """Initialize; see class docstring."""
+        self.fn = fn
+
+    def __call__(self, x: Set[ElementType]) -> Set[ElementType]:
+        """Modify the input set."""
+        elements = self.fn(x)
+        x.intersection_update(elements)
+        return x
